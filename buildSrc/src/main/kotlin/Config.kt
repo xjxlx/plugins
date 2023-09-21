e47cf46b7131c12503a5116d527230a8d5d8d91a
xjxliuxing@163.com <xjxliuxing@163.com>
2023-09-21 18:06:43 +0800

    publish version plugin

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,5 +1,5 @@
 object Config {
     const val plugin_group = "io.github.xjxlx"
-    const val plugin_version_publish = "1.0.2"
+    const val plugin_version_publish = "1.0.3"
     const val plugin_version_catalog = "1.0.0"
 }
