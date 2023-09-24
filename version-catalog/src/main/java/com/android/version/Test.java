9f74c53c048751e784d2e147dd9512f0b1b9bc0e
xjxliuxing@163.com <xjxliuxing@163.com>
2023-09-24 22:21:13 +0800

    update publish

--- version-catalog/src/main/java/com/android/version/Test.java
+++ version-catalog/src/main/java/com/android/version/Test.java
@@ -22,33 +22,8 @@ public class Test {
 //        for (int i = 0; i < listGradleContent.size(); i++) {
 //            println("listGradleContent:" + listGradleContent.get(i));
 //        }
+
+        gradleUtil.readLibsVersions();
         gradleUtil.changeGradleFile(file);
-
-        // 获取依赖的内容
-        List<String> dependenciesList = mFileUtil.filterStartAndEnd(listGradleContent, "dependencies", "}");
-//        for (int i = 0; i < dependenciesList.size(); i++) {
-//            String dependencies = dependenciesList.get(i);
-//            // replace dependencies
-//            println("dependencies:" + dependencies);
-
-//            if (true) {
-//                println("dependencies: " + dependencies);
-//                String[] split = dependencies.split("\"");
-//
-//                if (split.length >= 1) {
-//                    String originalDependencies = split[1];
-//                    println("originalDependencies: " + originalDependencies);
-//                    String[] splitChild = originalDependencies.split(":");
-//                    String group = splitChild[0];
-//                    String name = splitChild[1];
-//                    println("[group]: " + group + " [name]: " + name);
-//
-//                    String newDependencies = "libs." + (name.replace("-", "."));
-//                    println("newDependencies: " + newDependencies);
-//                    String resultDependencies = split[0] + newDependencies + split[2];
-//                    println("resultDependencies: " + resultDependencies);
-//                }
-//            }
-//        }
     }
 }
