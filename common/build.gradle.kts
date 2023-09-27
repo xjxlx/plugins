c76a3f7ad980f2cc1b0cac48bd7819e949fe1355
xjxliuxing@163.com <xjxliuxing@163.com>
2023-09-27 10:37:21 +0800

    update fills

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -64,5 +64,5 @@ dependencies {
 
     implementation("org.json:json:20230227")// json 依赖库
     implementation("org.jsoup:jsoup:1.16.1") // html依赖库
-    implementation(project(":pluginUtil"))
+    implementation(project(mapOf("path" to ":common:pluginUtils")))
 }
--- common/src/main/java/com/android/common/CommonTask.java
+++ common/src/main/java/com/android/common/CommonTask.java
@@ -1,5 +1,7 @@
 package com.android.common;
 
+import com.plugin.utils.SystemUtil;
+
 import org.gradle.api.DefaultTask;
 import org.gradle.api.provider.Provider;
 import org.gradle.api.tasks.TaskAction;
@@ -8,7 +10,7 @@ public class CommonTask extends DefaultTask {
 
     @TaskAction
     public void taskAction() {
-
+        SystemUtil.println("");
     }
 
     public int convertVersion(Provider<String> version) {
