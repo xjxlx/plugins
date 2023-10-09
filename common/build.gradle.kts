ef95b3f6f17a74594f9e73757cc9b5bec57f13b7
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-09 17:38:12 +0800

    update fills

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,16 +1,7 @@
-@Suppress("DSL_SCOPE_VIOLATION") plugins {
-    id("java-library")
+plugins {
     id("java-gradle-plugin")
     id("maven-publish")
     id("com.gradle.plugin-publish") version "1.0.0-rc-1" // 这个是发布到插件门户网站的插件
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
-
-    withJavadocJar()
-    withSourcesJar()
 }
 
 group = Config.plugin_group
@@ -37,13 +28,13 @@ gradlePlugin {
             displayName = "CommonPlugin"
             // 插件的描述
             description = "A collection of tool classes used to help develop plugins"
-            implementationClass = "com.android.common.CommonPlugin"
+            implementationClass = "com.android.utils.plugin.CommonPlugin"
         }
     }
 }
 
 dependencies {
-    // implementation("com.android.tools.build:gradle-api:7.4.0")
+    implementation("com.android.tools.build:gradle-api:7.4.2")
     implementation(gradleApi()) // gradle sdk
 
     implementation("org.json:json:20230227")// json 依赖库
--- common/src/main/java/com/android/common/utils/PrintlnUtil.java
+++
@@ -1,9 +0,0 @@
-package com.android.common.utils;
-
-import org.gradle.api.provider.Provider;
-
-public class PrintlnUtil {
-    public static void println(Provider<String> content) {
-        System.out.println(content.get());
-    }
-}
