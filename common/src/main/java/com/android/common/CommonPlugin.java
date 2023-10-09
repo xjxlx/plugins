9035ca1acf9034d8a9c2959e0cef69eeef652c0a
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-09 15:06:06 +0800

    update fills

--- common/src/main/java/com/android/common/CommonPlugin.java
+++ common/src/main/java/com/android/common/CommonPlugin.java
@@ -1,20 +1,12 @@
 package com.android.common;
 
 
-import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.provider.Provider;
-import org.gradle.api.tasks.TaskProvider;
 
 public class CommonPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
-        // println("common plugin apply ---->");
-        TaskProvider<CommonTask> versionTask = project.getTasks().register("commonPlugin", CommonTask.class, new Action<CommonTask>() {
-            @Override
-            public void execute(CommonTask version) {
-            }
-        });
+
     }
 }
