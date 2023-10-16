e6bd279a1f5b026d9b422525c4bf7c7fe250faf1
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-17 01:47:05 +0800

    update files

--- version/src/main/java/com/android/version/VersionPlugin.kt
+++ version/src/main/java/com/android/version/VersionPlugin.kt
@@ -6,45 +6,44 @@ import utils.Config
 import utils.FileUtil
 import utils.GradleUtil2
 import java.io.File
-import java.io.InputStream
 
 abstract class VersionPlugin : Plugin<Project> {
 
-    private val mLocalFileInputStream: InputStream? by lazy {
-        return@lazy GradleUtil2.getJarInputStream(this@VersionPlugin::class.java)
+    companion object {
+        private const val version = "version"
     }
 
     override fun apply(project: Project) {
-        println("version plugin apply --->")
-
         // 1：创建写入的task
-        project.task("versionGradleTask") { task ->
-            task.group = "install"
-            task.doLast {
-                // 从云端写入到本地
-                val gradleFile = File(project.gradle.gradleHomeDir, "libs.versions.toml")
+        project.task("tomlTask") { tomlTask ->
+            tomlTask.group = version
+            tomlTask.doLast {
+                // 1：从云端写入到本地
+                val gradleFile = File(project.rootDir, "${File.separator}gradle${File.separator}${GradleUtil2.libsVersions}")
+                println("gradleFile: ${gradleFile.absolutePath}")
                 GradleUtil2.writeGradleToLocal(Config.URL_VERSION_PATH, gradleFile)
-
-                // 2: 直接写入本地
-//                mLocalFileInputStream?.let {
-//                    FileUtil.writeFile(File(project.rootDir, "gradle" + File.separator + "libs.versions.toml"), it)
-//                    println("local version write success !")
-//                }
             }
         }
 
-        // 2: 创建转换的task
-        project.task("convertGradleTask") { task ->
-            task.doLast {
-                task.group = "build"
+        // 2: 转换module task
+        project.task("convertTask") { convertTask ->
+            convertTask.group = version
+            convertTask.doLast {
                 // 1: 读取本地的libs文件
                 GradleUtil2.initGradle(project)
 
                 // 2: 改变当前build的文件内容
-//                FileUtil.getModelGradlePath()
-                val modelGradlePath = FileUtil.getModelGradlePath(project)
-                println("modelGradlePath:$modelGradlePath")
-//                changeModuleGradleFile
+                GradleUtil2.changeGradleFile(FileUtil.getModelGradlePath(project))
+            }
+        }
+
+        // 3：转换root.gradle task
+        project.task("convertRootTask") { convertRootTask ->
+            convertRootTask.group = version
+            convertRootTask.doLast {
+
+                val dependencies = project.buildscript.dependencies
+
             }
         }
     }
