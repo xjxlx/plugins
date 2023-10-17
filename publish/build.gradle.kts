77b3827cf04bbf521bb88b1f782bf0fb7fd0e78c
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-18 00:16:06 +0800

    update files

--- publish/build.gradle.kts
+++ publish/build.gradle.kts
@@ -24,7 +24,7 @@ gradlePlugin {
     // 捆绑包中的每个插件都在块中指定。由于您此时只发布一个插件，因此只会有 一个条目，但如果您的项目将来发布捆绑包，您将在此处列出每个条目。plugins
     plugins {
         // 每个插件块的名称不会影响插件配置，但对于提供的每个插件需要是唯一的。
-        create("publish") {
+        create(Plugin.PUBLISH) {
             // 	设置插件的唯一性。id
             id = "${Plugin.GROUP}.${Plugin.PUBLISH}"
             // 短名称显示
