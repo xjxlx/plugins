838366e2b0c28b214356097a100450cc23ca02a1
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-18 23:22:41 +0800

    EA-209: update rsi ac funcation

--- catalog/src/main/java/com/android/version/VersionPlugin.kt
+++ catalog/src/main/java/com/android/version/VersionPlugin.kt
@@ -5,19 +5,14 @@ import org.gradle.api.Project
 import utils.Config
 import utils.FileUtil
 import utils.GradleUtil2
-
 import java.io.File
 
 abstract class VersionPlugin : Plugin<Project> {
 
-    companion object {
-        private const val version = "version"
-    }
-
     override fun apply(project: Project) {
-        // 1：创建写入的task
+        // 2：创建写入的task
         project.task("tomlTask") { tomlTask ->
-            tomlTask.group = version
+            tomlTask.group = Config.Plugin.CATALOG
             tomlTask.doLast {
                 // 1：从云端写入到本地
                 val gradleFile = File(project.rootDir, "${File.separator}gradle${File.separator}${GradleUtil2.libsVersions}")
@@ -26,13 +21,12 @@ abstract class VersionPlugin : Plugin<Project> {
             }
         }
 
-        // 2: 转换module task
+        //  2: 转换module task
         project.task("convertTask") { convertTask ->
-            convertTask.group = version
+            convertTask.group = Config.Plugin.CATALOG
             convertTask.doLast {
                 // 1: 读取本地的libs文件
                 GradleUtil2.initGradle(project)
-
                 // 2: 改变当前build的文件内容
                 GradleUtil2.changeGradleFile(FileUtil.getModelGradlePath(project))
             }
@@ -40,22 +34,16 @@ abstract class VersionPlugin : Plugin<Project> {
 
         // 3：转换root.gradle task
         project.task("convertRootTask") { convertRootTask ->
-            convertRootTask.group = version
+            convertRootTask.group = Config.Plugin.CATALOG
             convertRootTask.doLast {
-                val dependencies = project.buildscript.dependencies
-
-                project.buildscript.dependencies.components
-                project.buildscript.dependencies.components.all {
-                    println("all:${it}")
+                try {
+                    // 1: 读取本地的libs文件
+                    GradleUtil2.initGradle(project)
+                    // 2：改变本地root.gradle
+                    GradleUtil2.changeRootGradle()
+                } catch (e: Exception) {
+                    println("e:$e")
                 }
-
-
-                println("dependencies:${dependencies}")
-                val modules = dependencies.modules
-                println("modules:${modules}")
-                val localGroovy = dependencies.localGroovy()
-                println("localGroovy:${localGroovy}")
-
             }
         }
     }
