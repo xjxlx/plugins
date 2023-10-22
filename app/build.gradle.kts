bc9a6b4ae01501c37a2cb5d29ee4a9c88a1b28b8
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-22 11:44:30 +0800

    update files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,22 +1,20 @@
-plugins {
+@Suppress("DSL_SCOPE_VIOLATION") plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-//    id("com.android.apphelper.publish")
+    id("io.github.xjxlx.common")
 }
-//id 'com.uploadplugin'//3、此处为插件resources下文件名
 
 android {
     namespace = "com.android.plugins"
     compileSdk = 30
-//    compileSdk = convertVersion(libs.versions.compileSdks)
 
     defaultConfig {
         applicationId = "com.android.plugins"
-        minSdk = 26
-//        targetSdk = convertVersion(libs.versions.targetSdk) //30
-        targetSdk = 26
-        versionCode = 1
-        versionName = "1.0"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        // targetSdk = 26
+        versionCode = libs.versions.versionCode.get().toInt()
+        versionName = libs.versions.versionName.get()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -24,9 +22,12 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -44,9 +45,4 @@ dependencies {
     implementation(libs.core.ktx) {
         isForce = true
     }
-}
-
-fun convertVersion(version: Provider<String>): Int {
-    return version.get()
-        .toInt()
 }
