c49b9234c12b45dd2d891e12e5554e964786ff76
xjxliuxing@163.com <xjxliuxing@163.com>
2023-10-24 00:23:42 +0800

    update files

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -26,7 +26,7 @@ gradlePlugin {
     // 捆绑包中的每个插件都在块中指定。由于您此时只发布一个插件，因此只会有 一个条目，但如果您的项目将来发布捆绑包，您将在此处列出每个条目。plugins
     plugins {
         // 每个插件块的名称不会影响插件配置，但对于提供的每个插件需要是唯一的。
-        create(commonId) {
+        create("maven") {
             // 	设置插件的唯一性。id
             id = "${groupId}.${commonId}"
             // 短名称显示
