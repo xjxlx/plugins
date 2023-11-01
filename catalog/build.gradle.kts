b4699d5b44ed3c11c7fde1f3806a56928eb34524
xjxliuxing@163.com <xjxliuxing@163.com>
2023-11-02 01:30:49 +0800

    update plugin

--- catalog/build.gradle.kts
+++ catalog/build.gradle.kts
@@ -3,7 +3,9 @@ plugins {
     id("org.jetbrains.kotlin.jvm") // 用kotlin语言来开发
     id("com.gradle.plugin-publish") version "1.0.0-rc-1" // 这个是发布到插件门户网站的插件
     `version-catalog` // 1:配置发布插件
-} // false : ALiYun , true: gradle
+}
+
+// false : ALiYun , true: gradle
 val switch = true
 val catalogId = "catalog"
 
@@ -17,9 +19,13 @@ if (switch) { // ----------------------------------------↓↓↓发布到gradl
         website = "https://github.com/xjxlx/plugins/blob/main/versionManager/README.md"
         vcsUrl = "https://github.com/xjxlx/plugins/tree/main/versionManager"
         tags = listOf("common", "android", "version", "versionManager")
-    } // 发布到gradle门户
-    gradlePlugin { // 捆绑包中的每个插件都在块中指定。由于您此时只发布一个插件，因此只会有 一个条目，但如果您的项目将来发布捆绑包，您将在此处列出每个条目。plugins
-        plugins { // 每个插件块的名称不会影响插件配置，但对于提供的每个插件需要是唯一的。
+    }
+
+    // 发布到gradle门户
+    gradlePlugin {
+        // 捆绑包中的每个插件都在块中指定。由于您此时只发布一个插件，因此只会有 一个条目，但如果您的项目将来发布捆绑包，您将在此处列出每个条目。plugins
+        plugins {
+            // 每个插件块的名称不会影响插件配置，但对于提供的每个插件需要是唯一的。
             create("maven") { // 	设置插件的唯一性。id
                 id = "${group}.${catalogId}" // 短名称显示
                 displayName = "versionManager" // 插件的描述
@@ -27,16 +33,19 @@ if (switch) { // ----------------------------------------↓↓↓发布到gradl
                 implementationClass = "com.android.catalog.CatalogPlugin"
             }
         }
-    } //</editor-fold>
+    }
+    //</editor-fold>
     // ----------------------------------------↑↑↑发布到gradle↑↑↑------------------------------------
-} else { // ----------------------------------------↓↓↓发布到阿里云↓↓↓------------------------------------
+} else {
+    // ----------------------------------------↓↓↓发布到阿里云↓↓↓------------------------------------
     //<editor-fold desc=" 发布到阿里云  ">
     // 2：配置发布的跟文件，这里可以配置.toml文件，也可以配置具体的信息，可以具体查看官网
     catalog {
         versionCatalog {
             from(files(File(rootDir, "gradle${File.separator}29${File.separator}libs.versions.toml")))
         }
-    } // 3：配置具体的发布信息以及远程地址
+    }
+    // 3：配置具体的发布信息以及远程地址
     afterEvaluate {
         publishing {
             publications {
@@ -65,7 +74,8 @@ if (switch) { // ----------------------------------------↓↓↓发布到gradl
                 }
             }
         }
-    } //</editor-fold>
+    }
+    //</editor-fold>
     // ----------------------------------------↑↑↑发布到阿里云↑↑↑------------------------------------
 }
 
