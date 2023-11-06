c933029e29c8505ed26cc4cfc9688b4d031c685d
xjxliuxing@163.com <xjxliuxing@163.com>
2023-11-06 11:40:06 +0800

    update files

--- publish/src/main/java/com/android/publish/Test.kt
+++ publish/src/main/java/com/android/publish/Test.kt
@@ -1,13 +1,11 @@
 package com.android.publish
 
 import utils.FileUtil
-import java.io.File
 
 object Test {
 
     @JvmStatic
-    fun main(args: Array<String>) {
-//        HtmlUtil.getHtmlForGithubJsonArray(CatalogPlugin.ORIGIN_VERSION)
+    fun main(args: Array<String>) = //        HtmlUtil.getHtmlForGithubJsonArray(CatalogPlugin.ORIGIN_VERSION)
 //            ?.let {
 //                val jsonArrayToJsonObject = JsonUtil.arrayToObject(it)
 //                println("jsonArrayToJsonObject:$jsonArrayToJsonObject")
@@ -15,13 +13,9 @@ object Test {
 //                    println("sss:${obj}")
 //                }
 //            }
-
-        val gradleUserHomeDir = "/Users/XJX/.gradle"
-        val modules2 = File(gradleUserHomeDir, "caches/modules-2")
         FileUtil.iteratorsFile("/Users/XJX/.gradle/caches/modules-2", check = { file -> (file.isDirectory) }) { file ->
             if (file.name.startsWith("io.github.xjxlx")) {
                 println("file--->${file.absolutePath}")
             }
         }
-    }
 }
