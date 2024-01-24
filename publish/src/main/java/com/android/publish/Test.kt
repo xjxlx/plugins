85b626b102267d9a60f9e470657cb8cd4ee098d2
xjxliuxing@163.com <xjxliuxing@163.com>
2024-01-24 16:48:24 +0800

    update Html util

--- publish/src/main/java/com/android/publish/Test.kt
+++ publish/src/main/java/com/android/publish/Test.kt
@@ -1,11 +1,12 @@
 package com.android.publish
 
-import utils.FileUtil
+import utils.HtmlUtil
+import utils.JsonUtil
 
 object Test {
 
-    @JvmStatic
-    fun main(args: Array<String>) = //        HtmlUtil.getHtmlForGithubJsonArray(CatalogPlugin.ORIGIN_VERSION)
+//    @JvmStatic
+//    fun main(args: Array<String>) = //        HtmlUtil.getHtmlForGithubJsonArray(CatalogPlugin.ORIGIN_VERSION)
 //            ?.let {
 //                val jsonArrayToJsonObject = JsonUtil.arrayToObject(it)
 //                println("jsonArrayToJsonObject:$jsonArrayToJsonObject")
@@ -13,9 +14,18 @@ object Test {
 //                    println("sss:${obj}")
 //                }
 //            }
-        FileUtil.iteratorsFile("/Users/XJX/.gradle/caches/modules-2", check = { file -> (file.isDirectory) }) { file ->
-            if (file.name.startsWith("io.github.xjxlx")) {
-                println("file--->${file.absolutePath}")
-            }
-        }
+//        FileUtil.iteratorsFile("/Users/XJX/.gradle/caches/modules-2", check = { file -> (file.isDirectory) }) { file ->
+//            if (file.name.startsWith("io.github.xjxlx")) {
+//                println("file--->${file.absolutePath}")
+//            }
+//        }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val ORIGIN_VERSION =
+            "https://github.com/xjxlx/plugins/blob/master/publish/src/main/java/com/android/publish/version/version.json"
+        val htmlForGithubJsonArray = HtmlUtil.getHtmlForGithubJsonArray(ORIGIN_VERSION)
+        val arrayToObject = JsonUtil.arrayToObject(htmlForGithubJsonArray)
+        println("arrayToObject:$arrayToObject")
+    }
 }
